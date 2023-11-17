f8c1e42c52b4ac35ca5fd943037023d334a13fca
ompsingh1512 <150844513+ompsingh1512@users.noreply.github.com>
2023-11-17 15:06:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,13 +36,21 @@ try:
 except URLError as e:
   streamlit.error()
 #snowflake connector
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("fruit_load_list contains:")
-streamlit.dataframe(my_data_row)
+#streamlit.stop()
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+  
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  # my_cur = my_cnx.cursor()
+  # my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+  # my_data_row = my_cur.fetchall()
+  my_data_row = get_fruit_load_list()
+  #streamlit.header("fruit_load_list contains:")
+  streamlit.dataframe(my_data_row)
+streamlit.stop()  
 fadd=streamlit.text_input('What fruit would you like to add?','')
 streamlit.text(f"Thanks for adding {fadd}")
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
